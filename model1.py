ee603ca3268e231f47a5502eff954c2361e60c2f
logavani68 <logavanin@gmail.com>
2026-05-04 14:43:21 +0530

    Add files via upload

---
+++ model1.py
@@ -0,0 +1,82 @@
+#Elif ladder
+'''age=int(input("Enter your age:"))
+if age==18
+    print(" ready for voting")
+elif age>=19:
+    print(" already voted")
+else:
+    print(" not rights")'''
+    
+
+# 2 table program
+'''for i in range(1,16):
+    print(i,"X",2,"=",i*2)'''
+
+#loop program
+'''for i in range(100,2,-1):
+    print(i)'''
+
+
+
+
+#Nested if 
+'''age=int(input("Enter your age:"))
+weight=int(input("Enter your weight:"))
+if age>18:
+    if weight>50:
+        print("you can donate blood")
+    else:
+        print("weight is low you can't ")
+else:
+    print("age low can't donate")'''
+
+
+
+
+
+#Star program
+'''for i in range(i):
+    for j in range(i,i+1):
+        print("*",end="")
+    print()'''
+
+
+
+
+
+#Star in reverse order
+'''for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("\n")'''
+
+
+
+
+#Star in the order
+'''for i in range(5,0,-1):
+    for j in range(1,6-i+1):
+        print("*",end="")
+    print("\n")'''
+
+#Numbers 1 to 10
+'''for i in range(1,11):
+    print(i)'''
+
+
+
+#Even numbers
+'''for i in range(1,21):
+    if i%2==0:
+        print(i)'''
+
+
+#Odd numbers
+for i in range(1,100):
+    if i%2==1:
+        print(i,)
+
+
+
+
+
