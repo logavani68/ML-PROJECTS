9969714497d1a562ea3679c48623d1bb961859bb
logavani68 <logavanin@gmail.com>
2026-05-08 16:08:52 +0530

    Add files via upload

---
+++ Model 3.py
@@ -0,0 +1,72 @@
+# Numbers 1 to 10 using for loop
+'''for i in range(1,11):
+    print(i)'''
+
+# Numbers 1 to 10 using while loop
+'''while i<=10:
+    print(i)
+    i+=1'''
+
+#Even numbers 1 to 50
+'''for i in range(1,51):
+    if i%2==0:
+print(i)'''
+
+    
+#sum of numbers
+'''n=int(input("Enter a number:"))
+sum=0
+for i in range(n+1):
+    sum+=i
+print(sum)'''   
+
+
+
+#Multiplication Table
+'''for i in range(1,16):
+    print(i,"X",5,"=",i*5)'''
+
+
+'''n=int(input("Enter a number:"))
+for i in range(1,16):
+    print(n,"X",i,"=",n*i)'''
+
+
+
+#Factorial
+'''n=int(input("Enter a number:"))
+fact=1
+for i in range(1,n+1):
+    fact*=i
+print(fact)'''
+
+
+
+#reverse number
+'''n=int(input("Enter a number:"))
+for i in range(n,0,-1):
+    print(i,end=" ")'''
+
+
+#using for loop
+'''n=int(input("Enter a number:"))
+while n>0:
+    print(n,end=" ")
+    n-=1'''
+
+
+#count numbers
+n=int(input("Enter a number"))
+count=0
+for i in range(1,n+1):
+    count+=1
+print(count)    
+    
+
+    
+    
+
+
+
+
+
